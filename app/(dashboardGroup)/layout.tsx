875bf0d965f67ee62524e74dc092ffdb30f7a520
Sumaya <sheikhsumaya622@gmail.com>
2026-07-23 23:45:36 +0600

    all layout

---
+++ app/(dashboardGroup)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dashboardLayout = ({children} : {children : React.ReactNode}) => {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default dashboardLayout
