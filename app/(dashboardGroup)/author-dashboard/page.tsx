955cfa92fb4f01e651cbe0745436d118f99cd205
Sumaya <sheikhsumaya622@gmail.com>
2026-07-23 23:25:24 +0600

    doing project structure

---
+++ app/(dashboardGroup)/author-dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const authorDashboard = () => {
+  return (
+    <div>authorDashboard</div>
+  )
+}
+
+export default authorDashboard
---
+++ app/(publicGroup)/news/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const singleNewsPage = () => {
+  return (
+    <div>singleNewsPage</div>
+  )
+}
+
+export default singleNewsPage
