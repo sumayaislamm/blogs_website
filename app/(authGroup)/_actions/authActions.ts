2dcefa4bb4f626a4072ba2fc50ecfa9fd1a94f33
Sumaya <sheikhsumaya622@gmail.com>
2026-07-26 23:39:09 +0600

    cookies set for login user

--- app/(authGroup)/_actions/authActions.ts
+++ app/(authGroup)/_actions/authActions.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { cookies } from 'next/headers';
+
+
 
 // Login 
 type LoginState = {
@@ -29,8 +32,20 @@ export const loginAction = async (prevState : LoginState , formData: FormData) =
     },
     body: JSON.stringify(payload),
   });
-  const result = await res.json();
-  console.log(result);
+  const result : LoginState = await res.json();
+ if(result.success){
+  const cookieStore = await cookies()
+  cookieStore.set("accessToken", result.data.accessToken, {
+    httpOnly : true,
+    maxAge : 60 * 60 * 24 * 7 ,
+    sameSite : "lax", 
+  })
+  cookieStore.set("refreshToken", result.data.refreshToken, {
+    httpOnly : true,
+    maxAge : 60 * 60 * 24 * 7 ,
+    sameSite : "lax", 
+  })
+ }
   return result;
 };
 
