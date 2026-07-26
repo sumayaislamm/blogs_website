d4bbb009a2846fcc2bcba9e06a627b9c4d1c24ab
Sumaya <sheikhsumaya622@gmail.com>
2026-07-26 18:14:44 +0600

    login form client

---
+++ app/(authGroup)/_components/LoginFrom.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+const LoginFrom = () => {
+  return (
+    <form className="space-y-4">
+      <Card className="p-5 space-y-4">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Enter Your Email Here"
+          required
+        ></Input>
+        <Input
+          name="password"
+          type="password"
+          placeholder="Enter Your Password Here"
+          required
+        ></Input>
+        <Button type="submit">Login</Button>
+      </Card>
+    </form>
+  );
+};
+
+export default LoginFrom;
