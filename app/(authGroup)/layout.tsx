acca1ef0bf41063ce84b07e20765423d12f514be
Sumaya <sheikhsumaya622@gmail.com>
2026-07-26 18:20:39 +0600

    auth layout added

---
+++ app/(authGroup)/layout.tsx
@@ -0,0 +1,10 @@
+const AuthLayout = ({children}: {children : React.ReactNode}) => {
+  return (
+    <div className='max-w-7xl mx-auto'>
+        {children}
+    </div>
+    
+  )
+}
+
+export default AuthLayout
