7c1caebd9dfbcd09d5a280ab3485232f80818889
Sumaya <sheikhsumaya622@gmail.com>
2026-07-28 02:11:15 +0600

    prevent to go login page for login user

--- proxy.ts
+++ proxy.ts
@@ -1,19 +1,53 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+// import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "/register"];
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-    console.log("Proxy")
-  return NextResponse.redirect(new URL('/', request.url))
+export async function proxy(request: NextRequest) {
+    const pathname = request.nextUrl.pathname;
+//   const cookieStore = await cookies();
+//   const accessToken = cookieStore.get("accessToken");
+
+const accessToken = request.cookies.get("accessToken")?.value;
+
+
+const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+
+let userRole = null; 
+
+if(decodedToken){
+    userRole = decodedToken.role;
 }
- 
+if(accessToken && AUTH_ROUTES.includes(pathname)){
+    if(userRole === "USER"){
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+    else if(userRole === "AUTHOR"){
+        return NextResponse.redirect(new URL('/author-dashboard', request.url));
+    }
+    else if(userRole === "ADMIN"){
+        return NextResponse.redirect(new URL('/admin-dashboard', request.url));
+    }
+    else {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+}
+
+  return NextResponse.next();
+}
+
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
- 
+
 export const config = {
   matcher: [
-    '/dashboard/:path*',
-    '/author-dashboard/:path*',
-    '/admin-dashboard/:path*',
-  ]
-}
+    // "/dashboard/:path*",
+    // "/author-dashboard/:path*",
+    // "/admin-dashboard/:path*",
+    // "/login",
+    // "/register"
+    '/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)'
+  ],
+};
