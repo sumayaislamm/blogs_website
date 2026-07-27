37c06fc2649f4352ba4afbf0198098c56b5a923e
Sumaya <sheikhsumaya622@gmail.com>
2026-07-28 02:27:47 +0600

    Authenticating Protected Routes In Proxy

--- proxy.ts
+++ proxy.ts
@@ -4,36 +4,45 @@ import type { NextRequest } from "next/server";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 const AUTH_ROUTES = ["/login", "/register"];
-// This function can be marked `async` if using `await` inside
+// const PUBLIC_ROUTE = ["/", "/news", "/login", "/register"];
+const PUBLIC_ROUTE = ["/", "/news"];
 export async function proxy(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-//   const cookieStore = await cookies();
-//   const accessToken = cookieStore.get("accessToken");
+  const pathname = request.nextUrl.pathname;
+  //   const cookieStore = await cookies();
+  //   const accessToken = cookieStore.get("accessToken");
 
-const accessToken = request.cookies.get("accessToken")?.value;
+  const accessToken = request.cookies.get("accessToken")?.value;
 
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
 
-const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+  let userRole = null;
 
-let userRole = null; 
-
-if(decodedToken){
+  if (decodedToken) {
     userRole = decodedToken.role;
-}
-if(accessToken && AUTH_ROUTES.includes(pathname)){
-    if(userRole === "USER"){
-        return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
     }
-    else if(userRole === "AUTHOR"){
-        return NextResponse.redirect(new URL('/author-dashboard', request.url));
-    }
-    else if(userRole === "ADMIN"){
-        return NextResponse.redirect(new URL('/admin-dashboard', request.url));
-    }
-    else {
-        return NextResponse.redirect(new URL('/', request.url));
-    }
-}
+  }
+  const isPublicRoute = PUBLIC_ROUTE.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  //authenticated pages protection 
+  if(!accessToken && !isPublicRoute && !isAuthRoute){
+ return NextResponse.redirect(new URL('/login', request.url))
+  }
 
   return NextResponse.next();
 }
@@ -48,6 +57,6 @@ export const config = {
     // "/admin-dashboard/:path*",
     // "/login",
     // "/register"
-    '/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)'
+    "/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)",
   ],
 };
