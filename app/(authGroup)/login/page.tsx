d4740e119d3972812eab02117dfff35ebb13a388
Sumaya <sheikhsumaya622@gmail.com>
2026-09-08 23:32:05 +0600

    local

--- app/(authGroup)/login/page.tsx
+++ app/(authGroup)/login/page.tsx
@@ -9,7 +9,9 @@ const loginPage = () => (
         {/* Form Text  */}
         <div className="space-y-2 text-center">
           <h1 className="text-3xl font-bold">Welcome Back</h1>
-       
+          <p className="text-gray-500">
+            Enter your credentials to access your account
+          </p>
         </div>
 
         {/* Form  */}
